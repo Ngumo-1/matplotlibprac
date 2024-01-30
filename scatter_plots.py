093b1cb96c275aeb47395abae966c96a892ec502
Ngumo-1 <morrisngumo7@gmail.com>
2024-01-30 20:17:15 +0300

    created basic scatter plot with basic scatter plot concepts(alpha,color,etc)

---
+++ scatter_plots.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+x=np.random.rand(50)
+y1 = 2 * x + 1 + 0.1 * np.random.randn(50)
+y2 = -2 * x + 1 + 0.1 * np.random.randn(50)
+plt.scatter(x, y1, color='green', alpha=0.3, )
+plt.scatter(y1, y2, color='red', alpha=0.3)
+plt.show()
+# sizes=np.random.randint(100, size=(50))
+
