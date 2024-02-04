4c770ddf0f149878f44b6c2460d7e8882a3a447d
Ngumo-1 <morrisngumo7@gmail.com>
2024-02-04 22:30:13 +0300

    added shadow, colors, explode and startangle arguments

--- pies.py
+++ pies.py
@@ -6,5 +6,7 @@ from numpy import random
 random.seed(42)
 x=np.random.randint(40, size=(6,))
 labelss=["bananas","oranges","pears","guavas","apples","carrots"]
-plt.pie(x, labels=labelss)
+wedge_colors=["hotpink", "blue", "black", "yellow","grey","green"]
+explode_values=[0.1,0.1,0.1,0.1,0.1,0.1]
+plt.pie(x, labels=labelss, startangle=90, explode=explode_values, shadow=True, colors=wedge_colors)
 plt.show()
